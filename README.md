9e2bbc50b0b2db1f9e5f6c11018a4a69e92d76e3
John Corrigan <jcorrigan322@gmail.com>
2022-09-10 17:50:15 -0400

    Merge sort algorithm

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ### Computer Science Practice
 
 ### 1. Recursion
-###     1.1 Fibonacci Sequence Array using iteration and recursion
+### 1.1 Fibonacci Sequence Array using iteration and recursion
+### 1.2 Merge Sort
---
+++ recursion/mergeSort.js
@@ -0,0 +1,43 @@
+const arr = [8, 2, 3, 6, 5, 4, 7, 1, 100, 88, 99, 39999, -4, 10]
+
+function sort(arr){
+    if(arr.length <= 1) return arr;
+
+    const middle = Math.floor(arr.length/2)
+    // keeps calling sort on each arr until its one element
+    //recursion magic
+    let left = sort(arr.slice(0, middle))
+    let right = sort(arr.slice(middle))
+    //merges the two single element or empty arrays
+    return merge(left, right)
+}
+
+function merge(arr1, arr2){
+    const merged = [];
+    let i = 0;
+    let j = 0;
+    
+    while (i < arr1.length && j < arr2.length){
+        if(arr1[i] <= arr2[j]){
+            merged.push(arr1[i])
+            i++
+        }
+        else if(arr2[j] <= arr1[i]){
+            merged.push(arr2[j])
+            j++;
+        }
+    }
+    
+    while(i < arr1.length){
+        merged.push(arr1[i])
+        i++;
+    }
+    
+    while(j < arr2.length){
+        merged.push(arr2[j])
+        j++;
+    }
+    return merged
+}
+
+console.log(sort(arr))
